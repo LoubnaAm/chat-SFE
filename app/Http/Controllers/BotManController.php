4127cef2a9049f53c737cd60995dd3f8175d31c6
Ayoub_Am <ayoubamtdi@gmail.com>
2024-05-06 00:01:14 +0100

    first commit

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+use Illuminate\Support\Facades\Log;
+
+class BotManController extends Controller
+{
+    public function handle()
+    {
+        $botman = app('botman');
+
+        $botman->hears('{message}', function ($botman, $message) {
+            if ($message === 'hi' || $message === 'Hi' || $message === 'HI' || $message === 'hello' || $message === 'Hello' || $message === 'HELLO') {
+                $this->askName($botman);
+            } else {
+                $botman->reply("Start a conversation by saying hi.");
+            }
+        });
+
+        $botman->listen();
+    }
+
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) use ($botman) {
+            $name = $answer->getText();
+            Log::info('Answer received: ' . $name);
+
+            try {
+                $botman->say('Nice to meet you ' . $name, $botman->getMessage()->getSender());
+                Log::info('Reply sent successfully.');
+            } catch (\Exception $e) {
+                Log::error('Error sending reply: ' . $e->getMessage());
+            }
+        });
+    }
+
+
+}
